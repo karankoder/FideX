f401a9ce8ab6c595671c4a3c3791c897a54296dd
Karan Kumar Das <142100114+karankoder@users.noreply.github.com>
2025-03-27 17:53:53 +0530

    update socket address

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 // const socket = io("https://b8sxbhp0-3001.inc1.devtunnels.ms/");
-const socket = io('https://18zxdhrs-3001.inc1.devtunnels.ms/');
+const socket = io('https://0ddl624f-3000.inc1.devtunnels.ms/');
 export default socket;
